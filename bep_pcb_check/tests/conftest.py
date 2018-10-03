38e406dafd78878b64f7e635ffbf6d32abe56cb9
John ZuHone <jzuhone@gmail.com>
2018-10-03 13:27:47 -0400

    Refactor tests

--- bep_pcb_check/tests/conftest.py
+++ bep_pcb_check/tests/conftest.py
@@ -1,9 +1,2 @@
-import pytest
+from acis_thermal_check.conftest import *
 
-def pytest_addoption(parser):
-    parser.addoption("--generate_answers", 
-        help="Generate new answers, but don't test. Argument is the directory to store the answers to.")
-
-@pytest.fixture()
-def generate_answers(request):
-    return request.config.getoption('--generate_answers')
